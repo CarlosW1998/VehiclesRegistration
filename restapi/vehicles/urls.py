4115189b89e6a7afa832f86122e9e37a8aa6b0b4
Carlos <you@example.com>
2021-04-26 16:44:04 -0300

    fix urls

--- restapi/vehicles/urls.py
+++ restapi/vehicles/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework_nested import routers
 from vehicles.views import VehicleViewSet
+
 vehicle = routers.SimpleRouter()
 vehicle.register(
     r'vehicle',
     VehicleViewSet,
-    base_name='Vehicle operations'
 )
 
 urlpatterns = [

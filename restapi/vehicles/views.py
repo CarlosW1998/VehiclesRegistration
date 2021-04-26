b3193465af742c64a131e9a25b93087b1c33e7b7
Carlos <you@example.com>
2021-04-26 16:30:42 -0300

    Create Vehiclees View

--- restapi/vehicles/views.py
+++ restapi/vehicles/views.py
@@ -8,6 +8,6 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Vehicles to be viewed or edited.
     """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    queryset = Vehicle.objects.all()
+    serializer_class = VehiclesSerializer
+    #permission_classes = [permissions.IsAuthenticated]

80aac066c358f0e93e222d914bb27da4d13411e0
Carlos <you@example.com>
2021-04-26 16:37:39 -0300

    add url in path

--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('vehicles.urls')),
 ]

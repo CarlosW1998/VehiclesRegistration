e8a1bc0594108f23cae84c544f9f2e3e83e691a7
Carlos <you@example.com>
2021-04-26 16:12:01 -0300

    Create Vehicle Model

--- restapi/vehicles/models.py
+++ restapi/vehicles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Vehicle(models.Model):
+    model = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    price = models.FloatField()
+    note = brand = models.CharField(max_length=500)
+    
